f128afdc663efd91cac42bb96b48f81f4d6ff9bd
Mohamed-Marzaban <162023765+Mohamed-Marzaban@users.noreply.github.com>
2025-01-11 21:43:16 +0200

    finished get pending orders/delete order/update order

--- backend/routes/organizationRoutes.js
+++ backend/routes/organizationRoutes.js
@@ -7,5 +7,8 @@ router.post('/sign-up', organizationController.signUp)
 router.post('/upload-orders', authMiddleware(['organization']), organizationController.upload.single('ordersFile'), organizationController.uploadOrders)
 router.post('/create-order', authMiddleware(['organization']), organizationController.createOrder)
 router.get('/orders', authMiddleware(['organization']), organizationController.viewAllOrders)
+router.get('/pending-orders', authMiddleware(['organization']), organizationController.viewPendingPickUpOrders)
+router.delete('/order/:orderId', authMiddleware(['organization']), organizationController.deleteOrder)
+router.patch('/order/:orderId', authMiddleware(['organization']), organizationController.updateOrder)
 
 module.exports = router
